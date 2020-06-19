1c431ed7d2d2663b88a8aa3b12b7a5714e154c7f
kanata317 <y_tanaka317@yahoo.co.jp>
2020-06-20 00:25:28 +0900

    fix async function before it does not work

--- index.js
+++ index.js
@@ -5,22 +5,24 @@ function attendance() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://id.jobcan.jp/users/sign_in?app_key=atd');
-  await page.type('#user_email', 'xxxxxxxxxx');
-  await page.type('#user_password', 'xxxxxxxxx');
+  await page.type('#user_email', 'xxxxx');
+  await page.type('#user_password', 'xxxx');
   await Promise.all([
     page.waitForNavigation(),
     page.click('input[class="form__login"]')
   ]);
-  await page.click('#adit-button-push');
-  await page.goto('https://ssl.jobcan.jp/employee/attendance');
+  await page.goto('https://ssl.jobcan.jp/employee/man-hour-manage');
+  await page.click('.btn-info');
+  await page.waitForSelector('.btn-default', {visible: true});
+  await page.select('select[name="color"]', 'red');
   await page.screenshot({path: 'screenshot.png'});
   await browser.close();
 };
 
 cron.schedule('0 59 9 * * *', () => {
   attendance();
-};
+})
 
 cron.schedule('0 1 19 * * *', () => {
   attendance();
-};
+})
