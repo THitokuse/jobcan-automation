9cd88949cf92cfbd3c95cccad09c5d2ccfa8f3d9
THitokuse <hasegawa.taichiro@gmail.com>
2020-06-19 18:49:23 +0900

    modify gitignore

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const puppeteer = require('puppeteer');
-const cron = require('node-cron');
 
-function attendance() {
+(async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://id.jobcan.jp/users/sign_in?app_key=atd');
-  await page.type('#user_email', 'xxxxxxxxxx');
-  await page.type('#user_password', 'xxxxxxxxx');
+  await page.type('#user_email', 'hasegawa@hitokuse.com');
+  await page.type('#user_password', 'hasegawa123');
   await Promise.all([
     page.waitForNavigation(),
     page.click('input[class="form__login"]')
@@ -15,12 +14,32 @@ function attendance() {
   await page.goto('https://ssl.jobcan.jp/employee/attendance');
   await page.screenshot({path: 'screenshot.png'});
   await browser.close();
-};
+})();
 
-cron.schedule('0 59 9 * * *', () => {
-  attendance();
-};
 
-cron.schedule('0 1 19 * * *', () => {
-  attendance();
-};
+// const puppeteer = require('puppeteer');
+// const cron = require('node-cron');
+
+// function attendance() {
+//   const browser = await puppeteer.launch();
+//   const page = await browser.newPage();
+//   await page.goto('https://id.jobcan.jp/users/sign_in?app_key=atd');
+//   await page.type('#user_email', 'xxxxxxxxxx');
+//   await page.type('#user_password', 'xxxxxxxxx');
+//   await Promise.all([
+//     page.waitForNavigation(),
+//     page.click('input[class="form__login"]')
+//   ]);
+//   await page.click('#adit-button-push');
+//   await page.goto('https://ssl.jobcan.jp/employee/attendance');
+//   await page.screenshot({path: 'screenshot.png'});
+//   await browser.close();
+// };
+
+// cron.schedule('0 59 9 * * *', () => {
+//   attendance();
+// };
+
+// cron.schedule('0 1 19 * * *', () => {
+//   attendance();
+// };
